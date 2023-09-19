e6372d06b17f3a0e6433888edccacddda2049d2d
Ram181999 <141997273+Ram181999@users.noreply.github.com>
2023-09-19 17:45:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	
+	
+  let obj = {};
+  for (let i = 0; i < str.length; i++) {
+    if (obj[str[i]] !== undefined) {
+      // obj["a"]
+      obj[str[i]] += 1;
+    } else {
+      obj[str[i]] = 1;
+    }
+  }
+  // {"a": 2, "b" : 1, "c" : 1}
+console.log(obj);
+  for (let j = 0; j < str.length; j++) {
+    if (obj[str[j]] == 1) { // i = 1 , str[1] = "b" , obj["b"] = 1
+      return str[j];
+    }
+  }
+
+  return null;
+	
+	
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
